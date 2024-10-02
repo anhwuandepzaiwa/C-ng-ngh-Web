b56532c359bdfa82f48535657d3c2966880d9c1f
Văn Hồng Quân <stu715105192@hnue.edu.vn>
2024-10-02 23:14:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a*b; 
+}
+
+console.log(add(5, 3)); // 8
+console.log(subtract(5, 3)); // 2
